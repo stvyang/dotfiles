ef0c5a56642d6b146e5e384ca46487bd6f7669ba
Steven Yang <stv.yang10@gmail.com>
2020-10-25 00:35:07 +0700

    feat: Add php_use command to easily switch php version

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -32,5 +32,12 @@ agr() {
   ag -0 -l "$1" | AGR_FROM="$1" AGR_TO="$2" xargs -r0 perl -pi -e 's/$ENV{AGR_FROM}/$ENV{AGR_TO}/g'
 }
 
+# Change PHP version
+php_use() {
+  sudo update-alternatives --set php "/usr/bin/php$1"
+  sudo update-alternatives --set php-config "/usr/bin/php-config$1"
+  sudo update-alternatives --set phpize "/usr/bin/phpize$1"
+}
+
 # Work
 source $DOTFILES/.work/functions.zsh
