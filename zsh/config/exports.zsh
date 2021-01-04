36309ed0cc0b852f5bda98884e1f3b03c546494a
Steven Yang <stv.yang10@gmail.com>
2021-01-04 10:35:46 +0700

    fix: Change optional work script name

--- zsh/config/exports.zsh
+++ zsh/config/exports.zsh
@@ -4,9 +4,6 @@ export PATH="$HOME/.local/bin:$PATH"
 # nvm
 export NVM_DIR="$HOME/.nvm"
 
-# Register Arcanist's "arc" as a command in PATH
-# export PATH="$PATH:$HOME/arcanist/arcanist/bin" Install first under work
-
 # Github
 export GITHUB_USERNAME="stvyang"
 export GITHUB_EMAIL="stv.yang10@gmail.com"
@@ -17,15 +14,12 @@ export WORKSPACE="$HOME/Code/Workspace"
 export DOTFILES="$HOME/Code/Personal/dotfiles"
 export SCRIPTS="$HOME/Code/Personal/scripts"
 
-# Golang
-export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/.go
-
 # Rust
 export PATH=$HOME/.cargo/bin:$PATH
 
-# Vault
-# export PATH=$PATH:$HOME/vault TODO: Install first under general
+# Golang
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/.go
 
 # Other
 export EDITOR='nvim'
