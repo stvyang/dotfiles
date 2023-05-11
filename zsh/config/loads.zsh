f17a650ae043e5666ec6fcb5ffe4d655773b831d
Steven Yang <stv.yang10@gmail.com>
2023-05-11 23:06:15 +0700

    feat(zsh): Remove lambda-pure installation, only init on startup

--- zsh/config/loads.zsh
+++
@@ -1,7 +0,0 @@
-# Loads lambda-pure configurations
-PURE_NODE_ENABLED=0
-PURE_GIT_UP_ARROW='↑'
-PURE_GIT_DOWN_ARROW='↓'
-
-# Intentionally load nvm for lazy loading to work
-vim --version > /dev/null
--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -6,10 +6,6 @@ source $HOME/antigen.zsh
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
-# Bundles lambda-pure theme
-antigen bundle mafredri/zsh-async
-antigen bundle stvyang/lambda-pure      # Temporary revert for commit https://github.com/marszall87/lambda-pure/commit/62323a6be3224f8bb57b3724d045ffda79ad277d
-
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 
