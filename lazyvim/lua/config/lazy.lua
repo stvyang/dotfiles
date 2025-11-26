136eb3cf9ba2dd73d4fe4a28f61ee99d180b7b44
Steven Yang <stv.yang10@gmail.com>
2025-11-26 16:38:36 +0700

    Remove update prompt

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -24,6 +24,6 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "onedark" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- automatically check for plugin updates (disabled - use :Lazy update manually)
+  checker = { enabled = false },
 })
